3765792ce6942b126c1bcd6a9611c6ec9d584c46
Sergey Koshelev <sergey.k@nopcommerce.com>
2026-08-13 12:42:24 +0300

    Code refactoring and change plugin version

--- Nop.Plugin.Shipping.Fedex/Services/FedexService.cs
+++ Nop.Plugin.Shipping.Fedex/Services/FedexService.cs
@@ -704,35 +704,36 @@ public class FedexService
         request.RequestedShipment.ShipDateStamp = shipTimestamp.ToString("yyyy-MM-dd"); // Shipping date and time
 
         var isInternational = !request.RequestedShipment.Shipper.Address.CountryCode.Equals(request.RequestedShipment.Recipient.Address.CountryCode, StringComparison.InvariantCultureIgnoreCase);
+        var isShipmentToIndia = request.RequestedShipment.Shipper.Address.CountryCode.Equals("IN", StringComparison.InvariantCultureIgnoreCase) && request.RequestedShipment.Recipient.Address.CountryCode.Equals("IN", StringComparison.InvariantCultureIgnoreCase);
+        
+        if (!isInternational && !isShipmentToIndia) 
+            return;
+
+        //for international shipments and shipments to India, FedEx requires customs commodity details.
         var commodityPieces = request.RequestedShipment.TotalPackageCount > 0 ? request.RequestedShipment.TotalPackageCount : 1;
 
-        //For shipments to India, FedEx requires customs commodity details.
-        if (isInternational || (request.RequestedShipment.Shipper.Address.CountryCode.Equals("IN", StringComparison.InvariantCultureIgnoreCase) &&
-            request.RequestedShipment.Recipient.Address.CountryCode.Equals("IN", StringComparison.InvariantCultureIgnoreCase)))
+        var commodity = new Commodity
         {
-            var commodity = new Commodity
+            Name = "1",
+            CountryOfManufacture = request.RequestedShipment.Shipper.Address.CountryCode,
+            NumberOfPieces = commodityPieces,
+            QuantityUnits = "PCS",
+            Quantity = commodityPieces,
+            CustomsValue = new Money
             {
-                Name = "1",
-                CountryOfManufacture = request.RequestedShipment.Shipper.Address.CountryCode,
-                NumberOfPieces = commodityPieces,
-                QuantityUnits = "PCS",
-                Quantity = commodityPieces,
-                CustomsValue = new Money
-                {
-                    Amount = (double)orderSubTotal,
-                    Currency = currencyCode
-                }
-            };
+                Amount = (double)orderSubTotal,
+                Currency = currencyCode
+            }
+        };
 
-            request.RequestedShipment.CustomsClearanceDetail = new RequestedShipmentCustomsClearanceDetail
+        request.RequestedShipment.CustomsClearanceDetail = new RequestedShipmentCustomsClearanceDetail
+        {
+            CommercialInvoice = new CommercialInvoice
             {
-                CommercialInvoice = new CommercialInvoice
-                {
-                    ShipmentPurpose = CommercialInvoiceShipmentPurpose.SOLD
-                },
-                Commodities = [ commodity ]
-            };
-        }
+                ShipmentPurpose = CommercialInvoiceShipmentPurpose.SOLD
+            },
+            Commodities = [ commodity ]
+        };
     }
 
     private static bool IsPackageTooHeavy(decimal weight)
